97c823361a3f434210e8ba480b12f4f528a5f7ae
Hao Tang <SA21225402@mail.ustc.edu.cn>
2026-01-26 05:31:42 +0800

    th

--- dendrite_cloud_computing/src/helper.py
+++ dendrite_cloud_computing/src/helper.py
@@ -103,7 +103,7 @@ def process_image(image, model, image_size, var_scale):
 
     # Ensure reconstruction is in valid range
     recon_img = inv_smooth_scale(recon)    # post processing
-    recon_img = postprocess_image(recon_img.detach().cpu().numpy()[0].transpose(1, 2, 0))
+    recon_img = postprocess_image(recon_img.detach().cpu().numpy()[0, 0])
     recon_img = cv2.resize(recon_img, (original_shape[1], original_shape[0]))
 
     # Get control parameters
