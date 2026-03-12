c6aae4f47e6b4c4da119f60cb435cb6e66ee10e5
Hao Tang <SA21225402@mail.ustc.edu.cn>
2026-03-12 22:55:32 +0800

    th

--- generative_ai/step2_train_vae.py
+++ generative_ai/step2_train_vae.py
@@ -442,7 +442,7 @@ if __name__ == "__main__":
 
     parser.add_argument("--epochs", type=int, default=1000)
     parser.add_argument("--batch_size", type=int, default=64)
-    parser.add_argument("--lr", type=float, default=1e-4)
+    parser.add_argument("--lr", type=float, default=5e-5)
     parser.add_argument("--lr_factor", type=float, default=0.75)
     parser.add_argument("--lr_patience", type=float, default=10)
     parser.add_argument("--seed", type=int, default=0)
@@ -457,21 +457,21 @@ if __name__ == "__main__":
     parser.add_argument("--mdn_hidden", type=int, default=256)
 
     # VAE losses
-    parser.add_argument("--beta", type=float, default=1e-3)
+    parser.add_argument("--beta", type=float, default=1e-4)
     parser.add_argument("--beta_warmup_ratio", type=float, default=0.2)   # how many epochs (ratio * total epochs) before reaching beta
 
     # weights
-    parser.add_argument("--gamma", type=float, default=1e-2)
+    parser.add_argument("--gamma", type=float, default=0)
     parser.add_argument("--gamma_warmup_ratio", type=float, default=0.2)
 
-    parser.add_argument("--phy_weight", type=float, default=1e-3)
-    parser.add_argument("--phy_alpha", type=float, default=5)   # interface
+    parser.add_argument("--phy_weight", type=float, default=1e-4)
+    parser.add_argument("--phy_alpha", type=float, default=2)   # interface
     parser.add_argument("--phy_beta", type=float, default=1)    # two phase
 
     parser.add_argument("--scale_weight", type=float, default=0.1)
 
     # confidence scaling
-    parser.add_argument("--var_scale", type=float, default=0.1)
+    parser.add_argument("--var_scale", type=float, default=0.01)
 
     parser.add_argument("--patience", type=int, default=100)
     parser.add_argument("--save_root", type=str, default="results")
